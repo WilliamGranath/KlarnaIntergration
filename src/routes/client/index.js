699bac831d069400fe73a79f117f19bb83862d8d
William <williamgranath@gmail.com>
2022-01-12 02:53:07 +0100

    first commit

---
+++ src/routes/client/index.js
@@ -0,0 +1,21 @@
+const app = require('../../loaders/express-handlebars');
+const { getFakeStoreCart } = require('../../services/server/fakestore')
+const { createOrder } = require('../../services/server/klarna');
+
+app.get('/checkout/:cart_id', async function (req, res, next) {
+	const cart_id = req.params.cart_id;
+	
+	const cartItems = JSON.parse(req.query.cartItems)
+
+	const completeCart= await getFakeStoreCart(cartItems)
+
+ 	const klarnaJsonResponse = await createOrder(completeCart);
+	const html_snippet = klarnaJsonResponse.html_snippet
+
+
+	res.render('checkout', {
+	 	klarna_checkout: html_snippet
+	 });
+});
+
+module.exports = app;
---
+++ src/services/server/klarna.js
@@ -0,0 +1,139 @@
+import { json } from 'express';
+import fetch from 'node-fetch';
+const { fakeStoreToKlarnaCart} = require('./fakestore')
+
+
+const BASE_URL = 'https://api.playground.klarna.com';
+const getCarts = require('../../static/carts');
+
+function getKlarnaAuth() {
+    const username = process.env.PUBLIC_KEY;
+    const password = process.env.SECRET_KEY;
+    const auth = 'Basic ' + Buffer.from(username + ':' + password).toString('base64');
+    return auth;
+}
+
+
+function formatCart(currentCart) {
+    currentCart.forEach((cartItem) =>{
+        cartItem.total_amount = cartItem.quantity * cartItem.unit_price;
+        cartItem.total_tax_amount = cartItem.total_amount - cartItem.total_amount * 10000 / (10000 + cartItem.tax_rate);
+    })
+    return currentCart
+}
+
+// 1. Add async createOrder function that returns Klarna response.json()
+async function createOrder(fakeStoreCart) {
+    
+    
+    const currentCart = fakeStoreToKlarnaCart(fakeStoreCart);
+
+    const formattedCart = formatCart(currentCart)
+    
+    let order_amount = 0;
+    let order_tax_amount = 0;
+
+    formattedCart.forEach(currentCartItem => {
+        order_amount += currentCartItem.total_amount
+        order_tax_amount += currentCartItem.total_tax_amount
+    });
+
+    const SHIPPING_LIMIT_EXPRESS = 1000 * 100;
+    const SHIPPING_PRICE_EXPRESS = 109 * 100;
+
+    let shipping_options = [{
+        "id": "express_priority",
+        "name": "EXPRESS 1-2 Days",
+        "price": 0,
+        "tax_amount": 0,
+        "tax_rate": 0,
+    }];
+  
+
+    if (order_amount > SHIPPING_LIMIT_EXPRESS  || order_amount === 0 ? 0 : SHIPPING_PRICE_EXPRESS) {
+        shipping_options[0].price = SHIPPING_PRICE_EXPRESS;
+    }
+
+
+
+    const path = "/checkout/v3/orders"
+    const auth = getKlarnaAuth();
+    
+    
+    
+    const url = BASE_URL + path; 
+    const method = "POST";
+    const headers = {
+        "Content-Type": "application/json",
+        "Authorization": auth
+    };
+    const body = {
+            "purchase_country": "SE",
+            "purchase_currency": "SEK",
+            "locale": "sv-se",
+            "order_amount": order_amount,
+            "order_tax_amount": order_tax_amount,
+            "order_lines": formattedCart,
+            "merchant_urls": {
+                "terms": "https://www.example.com/terms.html",
+                "checkout": "https://www.example.com/checkout.html",
+                "confirmation": `${process.env.CONFIRMATION_URL}/confirmation?order_id={checkout.order.id}`,
+                "push": "https://www.example.com/api/push"
+            },
+            "shipping_options": shipping_options,
+    }
+
+    const stringifiedBody = JSON.stringify(body);
+
+    const response = await fetch(url, {
+        method,
+        headers,
+        "body": stringifiedBody
+     });
+     const jsonResponse = await response.json();
+     
+     if(response.status === 200 || response.status === 201) {
+         return jsonResponse;
+     } else {
+         console.error("ERROR: ", jsonResponse);
+         return {
+             html_snippet: `<h1>Var vänlig kom tillbaka senare, tjänsten ligger nere för tillfälligt</h1>`
+         }
+     }
+
+
+}
+
+// 2. Add async retrieveOrder function that returns Klarna response.json()
+
+async function retrieveOrder(order_id) {
+    const path = "/checkout/v3/orders/"+order_id 
+    const auth = getKlarnaAuth();
+    
+    
+    
+    const url = BASE_URL + path; 
+    const method = "GET";
+    const headers = {
+        //"Content-Type": "application/json",
+        "Authorization": auth
+    };
+    
+    const response = await fetch(url, {
+        method,
+        headers,
+     });
+
+     if (response.status === 200 || response.status === 201) {
+        const jsonResponse = await response.json();
+        return jsonResponse;
+    } else {
+        console.error("ERROR: ", response.status, response.statusText);
+        return {
+            html_snippet: `<h1>${response.status} ${response.statusText}</h1>`
+        }
+    }
+}
+
+// 3. export createOrder and retrieveOrder below, and use them in api/client/index.js and api/client/confirmation.js
+module.exports = {getKlarnaAuth, createOrder, retrieveOrder};
